4bbe01989fd54d566592c0d2ff78fc125ecab52c
v2tAcodin <vanthaitcv@gmail.com>
2026-07-30 11:43:09 +0700

    Fix telegram sender

--- scripts/telegram_sender.py
+++ scripts/telegram_sender.py
@@ -1,16 +1,16 @@
 import requests
-
 from config import BOT_TOKEN, CHAT_ID
 
-
 def send_message(message):
-
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    requests.post(
+    response = requests.post(
         url,
         data={
             "chat_id": CHAT_ID,
             "text": message,
         },
     )
+
+    print("Telegram Status:", response.status_code)
+    print("Telegram Response:", response.text)
