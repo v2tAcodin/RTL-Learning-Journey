edfb99b71465efb19d2e43dffa06f2541e5a4adf
v2tAcodin <vanthaitcv@gmail.com>
2026-07-30 11:37:23 +0700

    Refactor mentor bot

---
+++ scripts/telegram_sender.py
@@ -0,0 +1,16 @@
+import requests
+
+from config import BOT_TOKEN, CHAT_ID
+
+
+def send_message(message):
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": message,
+        },
+    )
